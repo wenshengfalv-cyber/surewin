ecb242228f0eaa38f05899c0c82b27ab2e6113de
Sam Li <weisen.li@hotmail.com>
2026-07-09 14:29:03 +0000

    update company address

--- constant/text.ts
+++ constant/text.ts
@@ -1,4 +1,4 @@
 export const PHONE_NUMBER = '+1-365-739-8928';
 export const WECHAT_NUMBER = 'surewinlegal';
 export const WECHAT_QR = '/images/wechat.png';
-export const COMPANY_ADDRESS = 'TBA';
+export const COMPANY_ADDRESS = '203-1943 Ironoak Way Oakville, ON L6H 3V7';
